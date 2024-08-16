d62264389d28814a7911acfbcb190a46468dfd56
chinaza-max <mosesogbonna68@gmail.com>
2024-08-16 14:30:31 +0100

    test

--- src/app/api/blinksQuestions/route.js
+++ src/app/api/blinksQuestions/route.js
@@ -96,10 +96,10 @@ import {
         const payload = {   
             title: `Solana daily quiz (${questions.dataValues.answered ? 'ANSWERED':'UN-ANSWERED'})`,
             icon: new URL("/DailyQuiiz.jpg", requestUrl.origin).toString()/*`data:image/png;base64,${imageBuffer.toString('base64')}`,*/,
-            //description: `You have only one attempt for a new question.\n ${users.length>0?'Rank Top 3;':''} ${usersAsString}`,
+            description: `You have only one attempt for a new question.\n ${users.length>0?'Rank Top 3;':''} ${usersAsString}`,
             description: `short`,
             links: {
-             // actions: actionsArray,
+              actions: actionsArray,
             },
           };  
       
